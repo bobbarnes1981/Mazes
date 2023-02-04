994082fa603ef515769e41298afb1e98f63ccba8
bobbarnes1981 <robertbarnes1981@googlemail.com>
2023-02-04 10:51:02 +0000

    Add test projects

---
+++ MazeGenerator.Tests/AldousBroderTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MazeGenerator.Tests
+{
+    public class AldousBroderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
