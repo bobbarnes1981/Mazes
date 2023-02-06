8163166803526941976c8c5b2395fe63504b3fc6
bobbarnes1981 <robertbarnes1981@googlemail.com>
2023-02-06 09:16:28 +0000

    Add von neumann neighbours

--- MazeLibrary/Grid.cs
+++ MazeLibrary/Grid.cs
@@ -7,6 +7,13 @@ namespace MazeLibrary
         public int Width { get; private set; }
         public int Height { get; private set; }
 
+        private static Coordinates[] relativeVonNeumannNeighbours = new Coordinates[] {
+            new Coordinates(0, -1),
+            new Coordinates(-1, 0),
+            new Coordinates(1, 0),
+            new Coordinates(0, 1)
+        };
+
         private T[] _cells;
 
         private int getIndex(int x, int y)
@@ -48,6 +55,37 @@ namespace MazeLibrary
             {
                 _cells[getIndex(x, y)] = value;
             }
+        }
+
+        public T this[Coordinates coordinates]
+        {
+            get
+            {
+                return this[coordinates.X, coordinates.Y];
+            }
+            set
+            {
+                this[coordinates.X, coordinates.Y] = value;
+            }
+        }
+
+        public Coordinates[] VonNeumannNeighbourhood(int x, int y)
+        {
+            List<Coordinates> neighbours = new List<Coordinates>();
+            foreach (var coordinates in relativeVonNeumannNeighbours)
+            {
+                var neighbour = new Coordinates(x + coordinates.X, y + coordinates.Y);
+                if (ValidCoordinates(neighbour.X, neighbour.Y))
+                {
+                    neighbours.Add(neighbour);
+                }
+            }
+            return neighbours.ToArray();
+        }
+
+        public Coordinates[] VonNeumannNeighbourhood(Coordinates coordinates)
+        {
+            return VonNeumannNeighbourhood(coordinates.X, coordinates.Y);
         }
     }
 }
